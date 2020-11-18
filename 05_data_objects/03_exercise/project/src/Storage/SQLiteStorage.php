f6a7f07a91be19b6fc77f01302e1202e93fa140a
rriczku <kuba8rychlicki@gmail.com>
2020-11-18 19:01:32 +0100

    05 done v2

--- 05_data_objects/03_exercise/project/src/Storage/SQLiteStorage.php
+++ 05_data_objects/03_exercise/project/src/Storage/SQLiteStorage.php
@@ -8,38 +8,12 @@ use Config\Directory;
 
 
 
-class SQLiteStorage implements Storage
+class SQLiteStorage extends Database
 {
-    private $pdo;
-
     public function __construct()
-    {
-        $this->pdo=new PDO("sqlite:../storage/db.sqlite");
+    {   $this->pdo=new PDO("sqlite:../storage/db.sqlite");
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->exec("CREATE TABLE IF NOT EXISTS objects(`key` VARCHAR(30) PRIMARY KEY,`data` VARCHAR(120) )");
         $this->pdo->exec("DELETE FROM objects");
     }
-
-    public function store(Distinguishable $distinguishable) : void
-    {
-        $statement = $this->pdo->prepare("INSERT INTO objects VALUES (:key, :data)");
-        $statement->bindValue('key',$distinguishable->key());
-        $statement->bindValue('data',serialize($distinguishable));
-        $statement->execute();
-    }
-
-    public function loadAll(): array
-    {
-        $query = $this->pdo->query("SELECT * FROM objects");
-        $results=$query->fetchAll(PDO::FETCH_ASSOC);
-
-        $result=[];
-        foreach ($results as $a => $a_value) {
-            array_push($result,unserialize($a_value['data']));
-        }
-
-
-        return $result;
-    }
-
 }
