8395994bb3017d8b9c430a57a5c04b409d8a12c7
rriczku <kuba8rychlicki@gmail.com>
2020-11-25 17:52:57 +0100

    Ex 06 done

--- 06_dependency_manager/02_exercise/project/src/Storage/RedisStorage.php
+++ 06_dependency_manager/02_exercise/project/src/Storage/RedisStorage.php
@@ -3,24 +3,31 @@
 namespace Storage;
 
 use Concept\Distinguishable;
-// TODO: ...
+use Predis\Client;
 
 class RedisStorage implements Storage
 {
-    // TODO: ...
 
-    public function __construct()
-    {
-        // TODO: ...
+    private Client $predis;
+
+    public function __construct(){
+        $this->predis=new Client();
     }
 
     public function store(Distinguishable $distinguishable) : void
     {
-        // TODO: ...
+        $this->predis->set($distinguishable->key(),serialize($distinguishable));
     }
 
     public function loadAll(): array
     {
-        return []; // TODO: ...
+        $keys=$this->predis->keys('*');
+        $result=[];
+
+        foreach($keys as $key)
+        {
+            $result[]=unserialize($this->predis->get($key));
+        }
+        return $result;
     }
 }
